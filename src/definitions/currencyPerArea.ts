4b4aeb1c62cd9b1b13627e5ad3701c3d2921ea67
Julian Swerdlin <julianswer36@gmail.com>
2024-08-16 00:03:44 -0300

    feature(currency/area): add new currencies per ft2 and m2

--- src/definitions/currencyPerArea.ts
+++ src/definitions/currencyPerArea.ts
@@ -1,8 +1,18 @@
+/* eslint-disable @typescript-eslint/no-loss-of-precision */
 import { Measure, Unit } from './../index.js';
 export type CurrencyPerAreaUnits =
   | CurrencyPerAreaMetricUnits
   | CurrencyPerAreaImperialUnits;
 export type CurrencyPerAreaSystems = 'metric' | 'imperial';
+
+// !USD
+// !CAD
+// !EUR
+// !GBP
+// !AUD
+// *JPY
+// *SGD
+// *INR
 
 export type CurrencyPerAreaMetricUnits =
   | 'USD/mm2'
@@ -11,7 +21,10 @@ export type CurrencyPerAreaMetricUnits =
   | 'EUR/m2'
   | 'GBP/m2'
   | 'AUD/m2'
-  | 'CAD/m2';
+  | 'CAD/m2'
+  | 'JPY/m2'
+  | 'SGD/m2'
+  | 'INR/m2';
 export type CurrencyPerAreaImperialUnits =
   | 'USD/in2'
   | 'USD/yd2'
@@ -19,7 +32,10 @@ export type CurrencyPerAreaImperialUnits =
   | 'EUR/ft2'
   | 'GBP/ft2'
   | 'AUD/ft2'
-  | 'CAD/ft2';
+  | 'CAD/ft2'
+  | 'JPY/ft2'
+  | 'SGD/ft2'
+  | 'INR/ft2';
 
 const metric: Record<CurrencyPerAreaMetricUnits, Unit> = {
   'USD/m2': {
@@ -31,29 +47,29 @@ const metric: Record<CurrencyPerAreaMetricUnits, Unit> = {
   },
   'AUD/m2': {
     name: {
-      singular: 'Dollar per Square Meter',
-      plural: 'Dollar Square Meters',
+      singular: 'Australian Dollar per Square Meter',
+      plural: 'Australian Dollars Square Meters',
     },
     to_anchor: 1 / 1.4647936838096354,
   },
   'EUR/m2': {
     name: {
-      singular: 'Dollar per Square Meter',
-      plural: 'Dollar Square Meters',
+      singular: 'Euro per Square Meter',
+      plural: 'Euros Square Meters',
     },
     to_anchor: 1 / 0.9968797663313828,
   },
   'CAD/m2': {
     name: {
-      singular: 'Dollar per Square Meter',
-      plural: 'Dollar Square Meters',
+      singular: 'Canadian Dollar per Square Meter',
+      plural: 'Canadian DOllars Square Meters',
     },
     to_anchor: 1 / 1.3146823727387462,
   },
   'GBP/m2': {
     name: {
-      singular: 'Dollar per Square Meter',
-      plural: 'Dollar Square Meters',
+      singular: 'Pound Sterling per Square Meter',
+      plural: 'Pounds Sterling Square Meters',
     },
     to_anchor: 1 / 0.8629616845012081,
   },
@@ -70,6 +86,27 @@ const metric: Record<CurrencyPerAreaMetricUnits, Unit> = {
       plural: 'Dollar per Square Millimeters',
     },
     to_anchor: 1 / 1000000,
+  },
+  'JPY/m2': {
+    name: {
+      singular: 'Japanese Yen per Square Meter',
+      plural: 'Japanese Yens per Square Meters',
+    },
+    to_anchor: 1 / 144.83299999999995,
+  },
+  'SGD/m2': {
+    name: {
+      singular: 'Singapore Dollar per Square Meter',
+      plural: 'Singapore Dollars per Square Meters',
+    },
+    to_anchor: 1 / 1.36739487745097,
+  },
+  'INR/m2': {
+    name: {
+      singular: 'Indian Rupee per Square Meter',
+      plural: 'Indian Rupees per Square Meters',
+    },
+    to_anchor: 1 / 82.33276400000002,
   },
 };
 
@@ -122,6 +159,27 @@ const imperial: Record<CurrencyPerAreaImperialUnits, Unit> = {
       plural: 'Square Yards',
     },
     to_anchor: 9,
+  },
+  'JPY/ft2': {
+    name: {
+      singular: 'Japanese Yen per Square Foot',
+      plural: 'Japanese Yens per Square Feet',
+    },
+    to_anchor: 1 / 144.83299999999995,
+  },
+  'SGD/ft2': {
+    name: {
+      singular: 'Singapore Dollar per Square Foot',
+      plural: 'Singapore Dollars per Square Feet',
+    },
+    to_anchor: 1 / 1.36739487745097,
+  },
+  'INR/ft2': {
+    name: {
+      singular: 'Indian Rupee per Square Foot',
+      plural: 'Indian Rupees per Square Feet',
+    },
+    to_anchor: 1 / 82.33276400000002,
   },
 };
 
